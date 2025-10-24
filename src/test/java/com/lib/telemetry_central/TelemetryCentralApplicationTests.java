592874a95d8b4b35b63ce399bbf9014fe6e98dac
Guilherme Mendes <guilherme_mendes2018@outlook.com>
2025-10-23 21:07:41 -0300

    Primeiro commit

---
+++ src/test/java/com/lib/telemetry_central/TelemetryCentralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lib.telemetry_central;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelemetryCentralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
